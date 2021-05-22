38c2014e76094b1119429b7427515d2732ca677e
brahimafa <brahimafassy@gmail.com>
2021-05-22 18:07:45 +0100

    fixed dropdown by @brahimAfa

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,8 +3,8 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
 const routes: Routes = [
-  { path: '', loadChildren: () => import('./client/client.module').then(m => m.ClientModule) },
-  { path: 'Client', loadChildren: () => import('./client/client.module').then(m => m.ClientModule) },
+  { path: '', loadChildren: () => import('./Client/client.module').then(m => m.ClientModule) },
+  { path: 'Client', loadChildren: () => import('./Client/client.module').then(m => m.ClientModule) },
 
 ];
 
--- src/app/sidebar/sidebar.component.ts
+++ src/app/sidebar/sidebar.component.ts
@@ -1,33 +1,44 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from '@angular/core'
 
 
 export interface RouteInfo {
-    path: string;
-    title: string;
-    icon: string;
-    class: string;
+  path: string;
+  id: String;
+  title: string;
+  icon: string;
+  class: string;
+  subMenus?: any
+
 }
 
 export const ROUTES: RouteInfo[] = [
-    { path: '/Client/Accueil',      title: 'Accueil',         icon:'nc-bank',       class: '' },
-    { path: '/Client/Ma_Relation_Banque',         title: 'Ma Relation Banque',             icon:'nc-diamond',    class: '' },
-    { path: '/Client/me-trasactions',          title: 'Mes Transactions',              icon:'nc-pin-3',      class: '' },
-    { path: '/Client/mes-demandes-en-ling', title: 'Mes Demandes Ligne',     icon:'nc-bell-55',    class: '' },
-    { path: '/user',          title: 'Me Deconnnecter',      icon:'nc-single-02',  class: '' },
-    { path: '/table',         title: 'Table List',        icon:'nc-tile-56',    class: '' },
-    { path: '/typography',    title: 'Typography',        icon:'nc-caps-small', class: '' },
-    { path: '/upgrade',       title: 'Upgrade to PRO',    icon:'nc-spaceship',  class: 'active-pro' },
+  { path: '/Client/Accueil', id: 'Accueil', title: 'Accueil', icon: 'nc-bank', class: '', subMenus:[
+    { path: '/Client/Accueil', id: 'Accueil', title: 'Accueil', icon: 'nc-bank', class: 'ml-3'},
+    { path: '/Client/Accueil', id: 'Accueil', title: 'Accueil', icon: 'nc-bank', class: 'ml-3'}
+  ] },
+  { path: '/Client/Ma_Relation_Banque', id: 'Ma', title: 'Ma Relation Banque', icon: 'nc-diamond', class: '' },
+  { path: '/Client/', id: 'Mes', title: 'Mes Transactions', icon: 'nc-pin-3', class: '' },
+  { path: '/Client/mes-demandes-en-ling', id: 'Mes', title: 'Mes Demandes Ligne', icon: 'nc-bell-55', class: '' },
+  { path: '/user', id: 'Mex', title: 'Me Deconnnecter', icon: 'nc-single-02', class: '' },
+  { path: '/table', id: 'Table', title: 'Table List', icon: 'nc-tile-56', class: '' },
+  { path: '/typography', id: 'Typography', title: 'Typography', icon: 'nc-caps-small', class: '' },
+  { path: '/upgrade', id: 'Upgrade', title: 'Upgrade to PRO', icon: 'nc-spaceship', class: 'active-pro' },
 ];
 
 @Component({
-    moduleId: module.id,
-    selector: 'sidebar-cmp',
-    templateUrl: 'sidebar.component.html',
+  moduleId: module.id,
+  selector: 'sidebar-cmp',
+  templateUrl: 'sidebar.component.html',
 })
 
 export class SidebarComponent implements OnInit {
-    public menuItems: any[];
-    ngOnInit() {
-        this.menuItems = ROUTES.filter(menuItem => menuItem);
-    }
+  public menuItems: any[];
+  public isCollapsed = true;
+  ngOnInit() {
+    this.menuItems = ROUTES.filter(menuItem => menuItem);
+  }
+  collapse() {
+    const navbar = document.getElementById('components');
+    navbar.classList.toggle("show");
+  }
 }
