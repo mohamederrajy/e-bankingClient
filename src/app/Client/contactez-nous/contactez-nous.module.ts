f5448b8b33afe76154d2343a44ad34c7d21dc74a
amine <aminepro924@gmail.com>
2021-06-11 16:46:43 +0100

    Recharge telecom and contact us are added succesfully

--- src/app/Client/contactez-nous/contactez-nous.module.ts
+++ src/app/Client/contactez-nous/contactez-nous.module.ts
@@ -3,13 +3,25 @@ import { CommonModule } from '@angular/common';
 
 import { ContactezNousRoutingModule } from './contactez-nous-routing.module';
 import { ContactezNousComponent } from './contactez-nous.component';
+import {FormsModule, ReactiveFormsModule} from '@angular/forms';
+import {NzTabsModule} from 'ng-zorro-antd/tabs';
+import {NzFormModule} from 'ng-zorro-antd/form';
+import {NzInputModule} from 'ng-zorro-antd/input';
+import {NzSelectModule} from 'ng-zorro-antd/select';
+
 
 
 @NgModule({
   declarations: [ContactezNousComponent],
   imports: [
     CommonModule,
-    ContactezNousRoutingModule
+    ContactezNousRoutingModule,
+    ReactiveFormsModule,
+    NzTabsModule,
+    NzFormModule,
+    NzInputModule,
+    NzSelectModule,
+    FormsModule
   ]
 })
 export class ContactezNousModule { }
--- src/app/Client/me-trasactions/recharge-telecom/recharge-telecom.module.ts
+++ src/app/Client/me-trasactions/recharge-telecom/recharge-telecom.module.ts
@@ -1,15 +1,29 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { NzTabsModule } from 'ng-zorro-antd/tabs';
+import { NzInputModule } from 'ng-zorro-antd/input';
+
 
 import { RechargeTelecomRoutingModule } from './recharge-telecom-routing.module';
 import { RechargeTelecomComponent } from './recharge-telecom.component';
+import {ReactiveFormsModule} from '@angular/forms';
+import {NzFormModule} from 'ng-zorro-antd/form';
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import {NzSelectModule} from 'ng-zorro-antd/select';
+
 
 
 @NgModule({
   declarations: [RechargeTelecomComponent],
   imports: [
     CommonModule,
-    RechargeTelecomRoutingModule
+    RechargeTelecomRoutingModule,
+    NzTabsModule,
+    ReactiveFormsModule,
+    NzFormModule,
+    NzSelectModule,
+    NzInputModule
   ]
 })
 export class RechargeTelecomModule { }
