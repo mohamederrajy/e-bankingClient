4919eb2f442db7b14bb9909993a147f28a7ca083
amine <aminepro924@gmail.com>
2021-06-12 03:30:55 +0100

    recharge part is added succesfully

--- src/app/Client/me-trasactions/recharge-telecom/recharge-telecom.component.ts
+++ src/app/Client/me-trasactions/recharge-telecom/recharge-telecom.component.ts
@@ -1,6 +1,11 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
 import { NzFormTooltipIcon } from 'ng-zorro-antd/form';
+import {CompteModel} from '../../../Core/Models/Compte-model/Compte-model.component';
+import {ClientService} from '../../../Core/Services/client-service/client-service.component';
+import {CompteService} from '../../../Core/Services/compte-service/compte-service.component';
+import {TransactionModel} from '../../../Core/Models/Transaction-model/Transaction-model.component';
+import {TransactionService} from '../../../Core/Services/transaction-service/transaction-service.component';
 
 
 
@@ -12,48 +17,52 @@ import { NzFormTooltipIcon } from 'ng-zorro-antd/form';
 })
 export class RechargeTelecomComponent implements OnInit {
   validateForm!: FormGroup;
+  loading: boolean = false;
+  errorMessage;
+  public comptes:CompteModel [];
+  private selectedOp;
+  public transaction:TransactionModel;
   private radioSelected: string;
-  private itemsList: ({
-    value: any;
-    name: string } | { name: string })[];
-  captchaTooltipIcon: NzFormTooltipIcon = {
-    type: 'info-circle',
-    theme: 'twotone'
-  };
+ private itemsList=[];
+  private saved: TransactionModel;
 
 
 
 
-  submitForm(): void {
+  submitForm(data:any): void {
+    this.transaction={
+   amount:data.Montant,
+      benificier:undefined,
+      account:data.Comptes,
+      description:"racharge"+this.selectedOp,
+      name:"Recharge de "+data.Offer+" pour  "+data.Tel,
+      type:"FACTEUR",
+      transactionType:undefined,
+
+    }
+    this.onSaveTransaction(this.transaction);
+    console.log(this.transaction)
+    console.log(data)
+    console.log(this.selectedOp)
     for (const i in this.validateForm.controls) {
       this.validateForm.controls[i].markAsDirty();
       this.validateForm.controls[i].updateValueAndValidity();
     }
   }
 
-  updateConfirmValidator(): void {
-    /** wait for refresh value */
-    Promise.resolve().then(() => this.validateForm.controls.checkPassword.updateValueAndValidity());
-  }
 
-  confirmationValidator = (control: FormControl): { [s: string]: boolean } => {
-    if (!control.value) {
-      return { required: true };
-    } else if (control.value !== this.validateForm.controls.password.value) {
-      return { confirm: true, error: true };
-    }
-    return {};
-  };
+
 
   getCaptcha(e: MouseEvent): void {
     e.preventDefault();
   }
 
-  constructor(private fb: FormBuilder) {
+  constructor(private fb: FormBuilder,private compteService:CompteService, private transactionService:TransactionService) {
 
    }
 
   ngOnInit(): void {
+    this.OnGetAccount()
     this.radioSelected = "item_3";
     this.itemsList=[
       {
@@ -62,22 +71,53 @@ export class RechargeTelecomComponent implements OnInit {
       {
         name:"inwi"
       },
+      {
+        name:"orange"
+      },
+      {
+        name:"ramsa"
+      },
     ]
     this.validateForm = this.fb.group({
-      email: [null, [Validators.email, Validators.required]],
-      password: [null, [Validators.required]],
-      checkPassword: [null, [Validators.required, this.confirmationValidator]],
-      nickname: [null, [Validators.required]],
-      phoneNumberPrefix: ['+86'],
-      phoneNumber: [null, [Validators.required]],
-      website: [null, [Validators.required]],
-      captcha: [null, [Validators.required]],
-      agree: [false]
+      Comptes: [Validators.required],
+      Tel: [Validators.required],
+      Offer: [Validators.required],
+      Montant: [Validators.required],
+
     })
   }
 
 
-  onItemChange(itemsListElement: {value: any; name: string} | {name: string}) {
+  onItemChange(value: any) {
+    this.selectedOp =value.name;
+
+  }
+  OnGetAccount(){
+    this.loading = true;
+    this.errorMessage = "";
+    this.compteService.GetComptes()
+      .subscribe(
+        (response) => {                           //next() callback
+          console.log('response received')
+          this.comptes = response;
+        },
+        (error) => {                              //error() callback
+          console.error('Request failed with error')
+          this.errorMessage = error;
+          this.loading = false;
+        },
+        () => {                                   //complete() callback
+          console.error('Request completed')      //This is actually not needed
+          this.loading = false;
+        })
+
+  }
+  onSaveTransaction (transaction:TransactionModel){
+    this.transactionService.SaveTransaction(transaction).subscribe(data => {
+      this.saved=data;
+      window.location.reload();
+      alert('succsess')
+    })
 
   }
 }
